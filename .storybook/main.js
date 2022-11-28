3153bd18c3e4d51f89e62cbdcd9c548883ac1bcd
Damien Nagle <damien.j.nagle@gmail.com>
2022-11-27 20:52:38 -0800

    Fixed absolute path resolutions in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": [
@@ -19,5 +21,13 @@ module.exports = {
   "framework": "@storybook/react",
   core: {
     builder: "webpack5"
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, ".."),
+      "node_modules",
+    ]
+
+    return config;
   }
 };
